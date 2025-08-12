f227eddbb17052f9ffeeb10e3cc56edd4b421321
Jose Zambrano <jzambrano@certicom.com.pe>
2025-08-11 22:21:09 -0500

    agregando empresa controller

---
+++ src/main/java/com/certicom/certifact_facturas_service_sp/controller/EmpresaController.java
@@ -0,0 +1,16 @@
+package com.certicom.certifact_facturas_service_sp.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/data/company")
+@RequiredArgsConstructor
+@Slf4j
+public class EmpresaController {
+
+
+
+}
