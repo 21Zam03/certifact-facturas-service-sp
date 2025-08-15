a1f722459eaa4b6d66c23ca1bac9aa6eaa771eb8
unknown <21zam03.free@gmail.com>
2025-08-14 22:58:53 -0500

    avanzando con el mapper de subidaregistroarchivo

--- src/main/java/com/certicom/certifact_facturas_service_sp/mapper/SubidaRegistroArchivoMapper.java
+++ src/main/java/com/certicom/certifact_facturas_service_sp/mapper/SubidaRegistroArchivoMapper.java
@@ -1,11 +1,13 @@
 package com.certicom.certifact_facturas_service_sp.mapper;
 
+import com.certicom.certifact_facturas_service_sp.dto.model.SubidaRegistroArchivoDto;
 import com.certicom.certifact_facturas_service_sp.entity.SubidaRegistroArchivoEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface SubidaRegistroArchivoMapper {
 
-    SubidaRegistroArchivoEntity regitrarSubidaArchivo();
+    int registrarSubidaArchivo(SubidaRegistroArchivoDto subidaRegistroArchivoDto);
+
 
 }
