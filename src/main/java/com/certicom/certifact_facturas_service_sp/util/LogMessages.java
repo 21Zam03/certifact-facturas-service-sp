bd067ca85512ec7f869e124eaa7d304dbe3032cc
Jose Zambrano <21zam03.free@gmail.com>
2026-01-05 11:42:41 -0500

    new update

--- src/main/java/com/certicom/certifact_facturas_service_sp/util/LogMessages.java
+++ src/main/java/com/certicom/certifact_facturas_service_sp/util/LogMessages.java
@@ -2,10 +2,10 @@ package com.certicom.certifact_facturas_service_sp.util;
 
 public class LogMessages {
 
-    public static final String ERROR_VALIDATION = "Error de validación";
-    public static final String SUCCESS_GET = "Consulta exitosa";
     public static final String ERROR_UNEXPECTED = "Error inesperado en la capa de servicio";
     public static final String ERROR_DATABASE = "Error en base de datos";
+    public static final String ERROR_VALIDATION = "Error de validación";
+    public static final String SUCCESS_GET = "Consulta exitosa";
 
     public static String currentMethod() {
         return Thread.currentThread().getStackTrace()[2].getMethodName();
